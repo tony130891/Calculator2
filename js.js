130cfd5f2e309bd09cf11a244fb01b696554ce94
Tony1308 <125692628+tony130891@users.noreply.github.com>
2025-12-31 08:42:07 -0400

    Instruction 7

--- js.js
+++ js.js
@@ -1,28 +1,41 @@
 let num1 = 0;
 let operator = "";
-let num2 = 0;
+let num2 = "";
 const display = document.querySelector('#inputBtn')
 const buttons = document.querySelectorAll('.op');
 const buttonsOp = document.querySelectorAll('.operator');
 const equal = document.querySelector('.equal');
+let result;
 
 
-function operate (operator,num1, num2) {
-    let result = 0;
- switch (operator) {
-    case '+':
-     result = (num1 + num2);
-    break;
-    case '-':
-     result = (num1 - num2);
-     break;
-    default:
-    case '*':
-     result = (num1 * num2);
-     break;
-     case '/':
-     result = (num1 / num2);
-     break;
+function add (num1, num2) {
+  return num1 + num2;  
+} 
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+if(num2 === 0) return undefined;
+    return num1 / num2;
+}
+
+function operate (num1, operator, num2) {
+ if (operator === '+') {
+    result = add(num1, num2);
+ } else if (operator === '-') {
+    result = subtract(num1, num2);
+ } else if (operator === '*') {
+    result = multiply(num1, num2);
+ } else if (operator === '/') {
+    result = divide(num1, num2);
+ } else {
+    result = 'invalid operator';
  }
  return result
 }
@@ -30,24 +43,33 @@ function operate (operator,num1, num2) {
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-       display.value += button.textContent;
-       if (operator.length === 0) {
+      if (operator.length === 0) {
+        display.value += button.textContent;
         num1 += button.textContent;
         num1 = Number(num1);
        } else {
+        display.value += button.textContent;
         num2 += button.textContent;
         num2 = Number(num2);
        }
     })
+    return num1, num2
 })
 
 buttonsOp.forEach((button) => {
     button.addEventListener('click', () => {
-        display.value += button.textContent;
-        operator += button.textContent;
+            display.value += button.textContent;
+            operator += button.textContent;
     })
+    return operator
 })
 
 equal.addEventListener('click', () => {
-    display.value = operate(operator, num1, num2);
-})
+   operate(num1, operator, num2);
+   display.value = result;
+
+   num1 = result;
+   operator = "";
+   // num2
+   num2 = "";
+});
